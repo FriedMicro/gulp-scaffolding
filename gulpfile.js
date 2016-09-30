493ee19d61e0ee6cd59d94ee4cada2990b3de357
Lucas Smith <friedmicrochips@gmail.com>
2016-09-30 13:58:54 -0400

    Create modified version for angularjs.

--- gulpfile.js
+++ gulpfile.js
@@ -1,50 +1,137 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
-var sass = require('gulp-sass');
-var prefix = require('gulp-autoprefixer');
-var sourcemaps = require('gulp-sourcemaps');
+var ngAnnotate = require('gulp-ng-annotate');
+var ngHtml2Js = require("gulp-ng-html2js");
+var minifyHtml = require("gulp-minify-html");
+var wrap = require('gulp-wrap');
+var clean = require('gulp-clean');
+var notify = require('gulp-notify');
+var size = require('gulp-filesize');
+var htmlMin = require('gulp-htmlmin');
 
-var paths = {
-	frontend:{
-		sass: {
-			src: './assets/scss/**/*.scss',
-			dest: './assets/css'
-		},
-		js: {
-			src: ['./assets/js/**/*.js', '!./assets/js/app.min.js', '!./assets/js/libraries/**/*.js'],
-			dest: './assets/js/',
-			filename:'app.min.js'
-		}
-	}
+var paths = {};
+paths.buildFolder = './dist';
+paths = {
+    frontend: {
+        buildFolder: paths.buildFolder,
+        sass: {
+            src: './sass/**/*.scss',
+            dest: paths.buildFolder
+        },
+        js: {
+            src: ['./js/**/*.js'],
+            dest: paths.buildFolder,
+            filename: 'app.min.js'
+        },
+        html: {
+            index_prod: './index.html',
+            index_dev: './index-dev.html',
+            prefix: '/',
+            src: './views/**/*.html',
+            dest: paths.buildFolder,
+            filename: 'partials.min.js'
+        }
+    }
 };
 
+gulp.task('styles', function() {
+    gulp.src(paths.frontend.sass.src)
+        .pipe(sass({
+            outputStyle: 'compressed'
+        }).on('error', sass.logError))
+        .pipe(gulp.dest(paths.frontend.sass.dest))
+});
+
 gulp.task('scripts', function() {
-	console.log('Concatenating Scripts');
-	return gulp.src(paths.frontend.js.src)
-		.pipe(sourcemaps.init())
-		.pipe(concat(paths.frontend.js.filename))
-		.pipe(uglify().on('error',handleError))
-		.pipe(sourcemaps.write())
-		.pipe(gulp.dest(paths.frontend.js.dest));
+    gulp.src(paths.frontend.js.src)
+        .pipe(concat(paths.frontend.js.filename))
+        .pipe(ngAnnotate())
+        .pipe(uglify().on('error', handleError))
+        .pipe(gulp.dest(paths.frontend.js.dest));
 });
 
-gulp.task('styles', function(){
-	console.log('Compiling Sass');
-	gulp.src(paths.frontend.sass.src)
-		.pipe(sourcemaps.init())
-		.pipe(sass({outputStyle: 'expanded'}).on('error',handleError))
-		.pipe(sourcemaps.write())
-		.pipe(prefix("last 2 versions", "> 1%", "ie 8", "ie 7"))
-		.pipe(gulp.dest(paths.frontend.sass.dest));
+gulp.task('partials', function() {
+    gulp.src(paths.frontend.html.src)
+        .pipe(minifyHtml({
+            empty: true,
+            spare: true,
+            quotes: true
+        }))
+        .pipe(ngHtml2Js({
+            moduleName: "Partials",
+            prefix: paths.frontend.html.prefix
+        }))
+        .pipe(concat(paths.frontend.html.filename))
+        .pipe(uglify())
+        .pipe(gulp.dest(paths.frontend.html.dest));
+})
+
+gulp.task('minify', function() {
+  return gulp.src(paths.frontend.html.index_prod)
+    .pipe(htmlMin({collapseWhitespace: true}))
+    .pipe(gulp.dest('dist'));
 });
 
-gulp.task('default', ['styles', 'scripts'], function(){
-	gulp.watch(paths.frontend.sass.src, ['styles']);
-	gulp.watch(paths.frontend.js.src, ['scripts']);
+gulp.task('styles-dev', function() {
+    gulp.src(paths.frontend.sass.src)
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest(paths.frontend.sass.dest))
 });
 
-function handleError(error){
-	console.log(error.toString());
-	this.emit('end');
+gulp.task('scripts-dev', function() {
+    gulp.src(paths.frontend.js.src)
+        .pipe(wrap('//<%= file.path %>\n<%= contents %>'))
+        .pipe(concat(paths.frontend.js.filename))
+        .pipe(ngAnnotate())
+        .pipe(gulp.dest(paths.frontend.js.dest));
+})
+
+gulp.task('partials-dev', function() {
+    gulp.src(paths.frontend.html.src)
+        .pipe(ngHtml2Js({
+            moduleName: "Partials",
+            prefix: paths.frontend.html.prefix
+        }))
+        .pipe(concat(paths.frontend.html.filename))
+        .pipe(gulp.dest(paths.frontend.html.dest));
+})
+
+gulp.task('minify-dev', function() {
+  return gulp.src(paths.frontend.html.index_dev)
+    .pipe(htmlMin({collapseWhitespace: false}))
+    .pipe(gulp.dest('dist'));
+});
+
+var getSize = function(){
+  gulp.src('./dist/*')
+  .pipe(size())
+}
+
+//Clean folder so we can have a clean upload
+gulp.task('clean', function () {
+  gulp.src(paths.frontend.buildFolder, {read: false})
+    .pipe(clean());
+});
+
+//Defaults to production
+gulp.task('default', ['styles', 'scripts', 'partials', 'minify'], function() {
+  getSize();
+    gulp.watch(paths.frontend.sass.src, ['styles']);
+    gulp.watch(paths.frontend.js.src, ['scripts']);
+    gulp.watch(paths.frontend.html.src, ['partials']);
+});
+
+//dev for local testing
+gulp.task('dev', ['styles-dev', 'scripts-dev', 'partials-dev', 'minify-dev'], function() {
+  getSize();
+    gulp.watch(paths.frontend.sass.src, ['styles-dev']);
+    gulp.watch(paths.frontend.js.src, ['scripts-dev']);
+    gulp.watch(paths.frontend.html.src, ['partials-dev']);
+})
+
+function handleError(error) {
+    notify(error.toString());
+    this.emit('end');
 }
